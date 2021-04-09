cb79cfdd69a203adc1aa3997e959f8fa5053e911
Carlene Maria Schlemper <carlenemari@gmail.com>
2021-04-09 16:47:10 -0300

    First class: Position

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,11 @@
+import boardGame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-	System.out.println("Projeto tabuleiro criado");
+	
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
